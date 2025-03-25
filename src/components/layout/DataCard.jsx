0e6e7e093ae5361071138558fe889a3465cb22ed
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-03-25 19:02:05 -0300

    Organização de código

--- src/components/layout/DataCard.jsx
+++ src/components/layout/DataCard.jsx
@@ -3,8 +3,8 @@ import styles from "./DataCard.module.css";
 function DataCard({ dataName, dataIcon, dataValue, iconColor }) {
   return (
     <figure className={styles.card_wrapper}>
-      <p>{dataValue || "nada"}</p>
-      <i style={{color: `${iconColor}`}}>{dataIcon}</i>
+      <p>{dataValue}</p>
+      <i style={{ color: `${iconColor}` }}>{dataIcon}</i>
       <figcaption>{dataName}</figcaption>
     </figure>
   );
