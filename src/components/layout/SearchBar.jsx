de284df25040f697b9ab197be42589f357b28188
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-03-30 18:07:15 -0300

    Implementa autocomplete na barra de pesquisa com Mapbox

--- src/components/layout/SearchBar.jsx
+++ src/components/layout/SearchBar.jsx
@@ -6,21 +6,21 @@ import styles from "./SearchBar.module.css";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
 
-const SearchBar = () => {
-  const [query, setQuery] = useState("");
+const SearchBar = ({ onLocationSelect }) => {
+  const [searchPlace, setSearchPlace] = useState("");
   const [results, setResults] = useState([]);
 
   useEffect(() => {
-    if (query) {
+    if (searchPlace) {
       async function getSuggestions() {
         try {
           const response = await fetch(
-            `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${mapboxgl.accessToken}`
+            `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchPlace}.json?types=place&access_token=${mapboxgl.accessToken}`
           );
           const data = await response.json();
 
           setResults(data.features);
-          console.log("Suggestions:", data.features);
+          console.log(data.features);
         } catch (err) {
           console.log("Error fetching suggestions:", err);
         }
@@ -30,17 +30,49 @@ const SearchBar = () => {
     } else {
       setResults([]); // Limpa as sugestôes se não houver texto no input
     }
-  }, [query]); // Roda sempre que query muda
+  }, [searchPlace]); // Roda sempre que searchPlace muda
+
+  const getSelectedPlace = (placeName) => {
+    setSearchPlace(placeName);
+  };
+
+  const getPlaceCoordinates = (city_name, latitude, longitude) => {
+    {
+      onLocationSelect(city_name, latitude, longitude);
+    }
+  };
 
   return (
     <div className={styles.search_wrapper}>
-      <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Pesquisar por local" />
-      <button>
+      <input
+        type="text"
+        value={searchPlace}
+        placeholder="Pesquisar por local"
+        onChange={(e) => setSearchPlace(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && results.length > 0) {
+            const firstResult = results[0];
+            getSelectedPlace(firstResult.place_name);
+            getPlaceCoordinates(firstResult.geometry.coordinates[1], firstResult.geometry.coordinates[0]);
+            setSearchPlace("");
+          }
+        }}
+      />
+      <i>
         <FaSearch className={styles.icon} />
-      </button>
+      </i>
       <ul>
         {results.map((result) => (
-          <li key={result.id}>{result.place_name}</li>
+          <li
+            key={result.id}
+            onClick={() => {
+              getSelectedPlace(result.place_name);
+              getPlaceCoordinates(result.place_name, result.geometry.coordinates[1], result.geometry.coordinates[0]);
+              setSearchPlace("");
+            }}
+          >
+            {result.place_name}
+          </li>
         ))}
       </ul>
     </div>
