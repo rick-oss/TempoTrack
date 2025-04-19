7e8863543e1c638cb4a34b806b6d059475dcac62
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-04-19 18:25:02 -0300

    feat: Adiciona componentização para cada dia para reutilização

---
+++ src/components/layout/OneDayCard.jsx
@@ -0,0 +1,25 @@
+import { FaTemperatureHigh, FaTemperatureLow } from "react-icons/fa";
+
+import styles from "./OneDayCard.module.css";
+
+function OneDayCard({ date_name, date, icon, temp, max_temp, min_temp, wind_speed }) {
+  return (
+    <div className={styles.card_wrapper}>
+      <h3>{date_name}</h3>
+      <p>{date}</p>
+      <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt="ícone do clima" />
+      <p>{temp}°C</p>
+      <p className={styles.temps}>
+        <FaTemperatureHigh size={20} color="#ff0000" className={styles.icons} />
+        {max_temp}°
+      </p>
+      <p className={styles.temps}>
+        <FaTemperatureLow size={20} color="#00ffff" className={styles.icons} />
+        {min_temp}°
+      </p>
+      <p className={styles.wind}>{wind_speed}</p>
+    </div>
+  );
+}
+
+export default OneDayCard;
