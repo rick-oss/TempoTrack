6e74822e93891e58f7ac351dbf7d66ff598c1d84
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-05-09 12:08:48 -0300

    feat: Implementa a troca da unidade de temperatura

--- src/components/layout/ForecastCard.jsx
+++ src/components/layout/ForecastCard.jsx
@@ -20,27 +20,25 @@ function ForecastCard({
       <div>
         <div className={styles.main_content}>
           <img src={`http://openweathermap.org/img/wn/${icon}@4x.png`} alt="Weather icon" />
-          <p className={styles.current_temp}>
-            {Math.floor(temp)}
-            <span className={styles.degree}>°C</span>
-          </p>
+          <p className={styles.current_temp}>{temp}</p>
+          <span className={styles.degree}>{unit === "metric" ? "°C" : "°F"}</span>
           <div className={styles.temp_icons}>
             <FaTemperatureHigh size={24} color="#ff0000" className={styles.hot_icon} />
             <FaTemperatureLow size={24} color="#00ffff" className={styles.cold_icon} />
           </div>
           <div className={styles.temp_content}>
             <p className={styles.temps}>
-              {Math.ceil(max_temp)}
+              {max_temp}
               <span className={styles.degree}>°</span>
             </p>
             <p className={styles.temps}>
-              {Math.floor(min_temp)}
+              {min_temp}
               <span className={styles.degree}>°</span>
             </p>
           </div>
           <div className={styles.weather_info}>
             <p className={styles.description}>{description}</p>
-            <p className={styles.feels_like}>Sensação térmica {Math.floor(feels_like)}°</p>
+            <p className={styles.feels_like}>Sensação térmica {feels_like}°</p>
           </div>
         </div>
       </div>
