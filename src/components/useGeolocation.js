2b7b8441268938591e747a6f9157a00ce3385cbe
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-05-09 18:14:26 -0300

    feat: Atualiza código para lidar com o conteúdo que será mostrado caso a permissão seja negada

--- src/components/useGeolocation.js
+++ src/components/useGeolocation.js
@@ -5,32 +5,29 @@ const useGeolocation = () => {
     latitude: null,
     longitude: null,
   });
-  const [error, setError] = useState(null);
+  const [permissionDenied, setPermissionDenied] = useState(false);
 
   useEffect(() => {
-    // Verifica se o navegador suporta geolocalização
-    if (!navigator.geolocation) {
-      setError("Geolocalização não é suportada pelo seu navegador.");
-      return;
-    }
-
     // Função de sucesso quando a geolocalização é obtida
     const getCoords = (position) => {
       setGeoLocation({
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
       });
+      setPermissionDenied(false);
     };
 
     // Função de erro caso o usuário não permita que algo aconteça
     const handleError = (error) => {
-      setError(`Erro ao pegar a localização: ${error.message}`);
+      if (error.code === error.PERMISSION_DENIED) {
+        setPermissionDenied(true);
+      }
     };
 
     navigator.geolocation.getCurrentPosition(getCoords, handleError);
   }, []);
 
-  return { geolocation, error };
+  return { geolocation, permissionDenied };
 };
 
 export default useGeolocation;
