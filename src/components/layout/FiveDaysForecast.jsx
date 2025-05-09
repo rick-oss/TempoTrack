36b6c42ae1123737518c5b2f6b6d2de25e9a8bc2
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-05-09 12:12:32 -0300

    feat: adiciona controle de loading com render seguro no card

--- src/components/layout/FiveDaysForecast.jsx
+++ src/components/layout/FiveDaysForecast.jsx
@@ -2,20 +2,21 @@ import OneDayCard from "./OneDayCard";
 
 import styles from "./FiveDaysForecast.module.css";
 
-function FiveDaysForecast({ cards }) {
+function FiveDaysForecast({ cards, loading, safeRender }) {
   return (
     <div className={styles.card_wrapper}>
       <h1>Previsão para 5 dias</h1>
-      {cards.map(({ dateName, date, icon, temp, maxTemp, minTemp, windSpeed }, index) => (
+      {cards.map(({ dateName, date, icon, temp, maxTemp, minTemp, windSpeed, unit }, index) => (
         <OneDayCard
           key={index}
           date_name={dateName}
           date={date}
           icon={icon}
-          temp={Math.round(temp)}
-          max_temp={Math.ceil(maxTemp)}
-          min_temp={Math.floor(minTemp)}
-          wind_speed={windSpeed}
+          temp={safeRender(loading, Math.round(temp), 18)}
+          max_temp={safeRender(loading, Math.ceil(maxTemp), 18)}
+          min_temp={safeRender(loading, Math.floor(minTemp), 18)}
+          wind_speed={safeRender(loading, windSpeed, 18)}
+          unit={unit}
         />
       ))}
     </div>
