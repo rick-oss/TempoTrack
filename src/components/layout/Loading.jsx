c28618e92a7e4b94778e6e21713ee2b4021e8eb6
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-05-23 18:04:18 -0300

    feat(Loading): adiciona suporte para customClass que melhora UI durante o carregamento

--- src/components/layout/Loading.jsx
+++ src/components/layout/Loading.jsx
@@ -2,11 +2,11 @@ import { FaSpinner } from "react-icons/fa";
 
 import styles from "./Loading.module.css";
 
-const Loading = ({ size }) => {
+const Loading = ({ size, customClass }) => {
   return (
-    <>
+    <div className={`${styles[customClass]}`}>
       <FaSpinner className={styles.spinner} size={size} />
-    </>
+    </div>
   );
 };
 
