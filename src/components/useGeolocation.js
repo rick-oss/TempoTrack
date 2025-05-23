3df196bf246d41871ba6a6ddc442488a618632b0
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-05-23 18:02:14 -0300

    Cria state para a verificação de loading

--- src/components/useGeolocation.js
+++ src/components/useGeolocation.js
@@ -6,6 +6,7 @@ const useGeolocation = () => {
     longitude: null,
   });
   const [permissionDenied, setPermissionDenied] = useState(false);
+  const [permissionStatusChecked, setPermissionStatusChecked] = useState(false);
 
   useEffect(() => {
     // Função de sucesso quando a geolocalização é obtida
@@ -15,19 +16,21 @@ const useGeolocation = () => {
         longitude: position.coords.longitude,
       });
       setPermissionDenied(false);
+      setPermissionStatusChecked(true);
     };
 
     // Função de erro caso o usuário não permita que algo aconteça
     const handleError = (error) => {
       if (error.code === error.PERMISSION_DENIED) {
         setPermissionDenied(true);
+        setPermissionStatusChecked(true);
       }
     };
 
     navigator.geolocation.getCurrentPosition(getCoords, handleError);
   }, []);
 
-  return { geolocation, permissionDenied };
+  return { geolocation, permissionDenied, permissionStatusChecked };
 };
 
 export default useGeolocation;
