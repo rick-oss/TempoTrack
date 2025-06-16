2dab0b2e77edd9fbf7ddce9bd69297a40def36a2
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-06-16 18:26:45 -0300

    refactor: adiciona wrapper com className para melhorar estrutura do layout

--- src/components/layout/FiveDaysForecast.jsx
+++ src/components/layout/FiveDaysForecast.jsx
@@ -6,19 +6,21 @@ function FiveDaysForecast({ cards, loading, safeRender }) {
   return (
     <div className={styles.card_wrapper}>
       <h1>Previsão para 5 dias</h1>
-      {cards.map(({ dateName, date, icon, temp, maxTemp, minTemp, windSpeed, unit }, index) => (
-        <OneDayCard
-          key={index}
-          date_name={dateName}
-          date={date}
-          icon={icon}
-          temp={safeRender(loading, Math.round(temp), 18)}
-          max_temp={safeRender(loading, Math.ceil(maxTemp), 18)}
-          min_temp={safeRender(loading, Math.floor(minTemp), 18)}
-          wind_speed={safeRender(loading, windSpeed, 18)}
-          unit={unit}
-        />
-      ))}
+      <div className={styles.card_content}>
+        {cards.map(({ dateName, date, icon, temp, maxTemp, minTemp, windSpeed, unit }, index) => (
+          <OneDayCard
+            key={index}
+            date_name={dateName}
+            date={date}
+            icon={icon}
+            temp={safeRender(loading, Math.round(temp), 18)}
+            max_temp={safeRender(loading, Math.ceil(maxTemp), 18)}
+            min_temp={safeRender(loading, Math.floor(minTemp), 18)}
+            wind_speed={safeRender(loading, windSpeed, 18)}
+            unit={unit}
+          />
+        ))}
+      </div>
     </div>
   );
 }
